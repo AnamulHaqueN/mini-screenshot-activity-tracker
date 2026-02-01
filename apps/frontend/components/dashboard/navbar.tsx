6ceac62e15fdd552806211d6b661c0feb0b40bb8
MArhamAA1422 <arham511624@gmail.com>
2026-02-01 13:24:35 +0600

    Routes to home if log out

--- apps/frontend/components/dashboard/navbar.tsx
+++ apps/frontend/components/dashboard/navbar.tsx
@@ -13,7 +13,7 @@ export default function Navbar() {
    const handleLogout = () => {
       logout(undefined, {
          onSuccess: () => {
-            router.push("/login")
+            router.push("/")
          },
       })
    }
