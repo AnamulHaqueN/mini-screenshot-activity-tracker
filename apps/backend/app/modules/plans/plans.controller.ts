a50627be2955ca92592b731f1b67f08900627e8a
MArhamAA1422 <arham511624@gmail.com>
2026-02-10 15:07:47 +0600

    Remove console logs from plans controller

--- apps/backend/app/modules/plans/plans.controller.ts
+++ apps/backend/app/modules/plans/plans.controller.ts
@@ -5,14 +5,12 @@ import { planValidator } from './plan.validator.js'
 export default class PlansController {
    async index({ response }: HttpContext) {
       const plans = await Plan.all()
-      console.log('here is my plans', plans)
       return response.ok({ data: plans })
    }
    async store({ request, response }: HttpContext) {
       const payload = await request.validateUsing(planValidator)
-
       const plan = await Plan.create(payload)
-      console.log('here is my payload', payload)
+
       return response.created({
          message: 'Plan created successfully',
          data: plan,
