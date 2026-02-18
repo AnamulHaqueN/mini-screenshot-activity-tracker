0bd0eb529abbf2cc85dbcc9f0452e1670d073425
MArhamAA1422 <arham511624@gmail.com>
2026-02-18 10:30:50 +0600

    Add both bunny CDN  and cloudinary hostname in next config

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -3,16 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-   //  remotePatterns: [
-   //    {
-   //      protocol: "https",
-   //      hostname: "res.cloudinary.com",
-   //    },
-   //  ],
     remotePatterns: [
       {
         protocol: "https",
         hostname: "ezystaff.b-cdn.net",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
       },
     ],
   },
